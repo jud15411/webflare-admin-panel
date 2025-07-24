e2a63bd05074f83de8b0fc0e5a0c76f297a788e8
jud15411 <117609861+jud15411@users.noreply.github.com>
2025-07-24 18:57:24 -0400

    Update businessSettingsRoutes.js

--- backend/routes/businessSettingsRoutes.js
+++ backend/routes/businessSettingsRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { hasRole } from '../middleware/roleMiddleware.js';
 // CORRECTED: Changed getBusinessSettings to getSettings
-import { getSettings, updateBusinessSettings, getPublicBusinessSettings } from '../controllers/businessSettingsController.js';
+import { getSettings, updateSettings} from '../controllers/businessSettingsController.js';
 
 const router = express.Router();
 
@@ -11,6 +11,6 @@ router.get('/public', getPublicBusinessSettings);
 router.route('/')
     // CORRECTED: Changed getBusinessSettings to getSettings
     .get(protect, hasRole('ceo'), getSettings)
-    .put(protect, hasRole('ceo'), updateBusinessSettings);
+    .put(protect, hasRole('ceo'), updateSettings);
 
 export default router;
